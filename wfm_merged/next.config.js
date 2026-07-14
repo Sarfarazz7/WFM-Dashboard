279f7e06694db34a62d6f412dfebcd8b6172a99f
Sarfarazz7 <sheikhsarfaraz1912@gmail.com>
2026-07-14 21:17:01 +0530

    chore: remove stale wfm_merged snapshot

--- wfm_merged/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Excel parsing (xlsx) runs inside API routes on Node runtime by default.
-};
-
-module.exports = nextConfig;
